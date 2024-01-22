b9e2bcb58d72b7ff1a1fb769a190e3c79ce62296
jakegodsall <jake.edward.godsall@gmail.com>
2024-01-22 19:09:05 +0000

    moved Header to the root layout

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -3,6 +3,8 @@ import "./globals.css";
 
 const kumbhSans = Kumbh_Sans({ subsets: ["latin"] });
 
+import Header from "./components/Header/Header";
+
 export const metadata = {
   title: "Devjobs Web App (Frontend Mentor)",
   description: "Solution to the Devjobs Web App challenge by Frontend Mentor",
@@ -11,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={kumbhSans.className}>{children}</body>
+      <body className={kumbhSans.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,6 @@
-import Header from "./components/Header/Header";
-
 export default function Home() {
   return (
     <div>
-      <Header />
       <h1>Main</h1>
     </div>
   );
