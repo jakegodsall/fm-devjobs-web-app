750255fffad2f587d02cd7bfa2df3095983f6ab5
jakegodsall <jake.edward.godsall@gmail.com>
2024-01-22 18:35:17 +0000

    added basic header component

---
+++ frontend/src/app/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import Logo from "@public/assets/desktop/logo.svg";
+
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <Image className={styles.header__logo} src={Logo} alt="devjobs logo" />
+    </header>
+  );
+}
