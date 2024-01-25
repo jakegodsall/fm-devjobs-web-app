438848123489c0d6a4c3c03da39d99f6d330af91
jakegodsall <jake.edward.godsall@gmail.com>
2024-01-25 17:07:01 +0000

    removed inner div of modal to allow for more customisation

--- frontend/src/app/components/UI/Modal/Modal.jsx
+++ frontend/src/app/components/UI/Modal/Modal.jsx
@@ -5,9 +5,5 @@ import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
 
 export default function Modal({ isOpen, onClose, children }) {
-  return (
-    <div className={styles.modalBackdrop}>
-      <div className={styles.modalContent}>{children}</div>
-    </div>
-  );
+  return <div className={styles.modalBackdrop}>{children}</div>;
 }
