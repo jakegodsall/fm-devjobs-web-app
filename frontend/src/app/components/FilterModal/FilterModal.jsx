ca176dc12d744bcf0811c97b7c7dad88a2c4275b
jakegodsall <jake.edward.godsall@gmail.com>
2024-01-27 18:11:01 +0000

    Added Full Time Only to the filter modal

--- frontend/src/app/components/FilterModal/FilterModal.jsx
+++ frontend/src/app/components/FilterModal/FilterModal.jsx
@@ -6,9 +6,16 @@ import styles from "./FilterModal.module.css";
 export default function FilterModal() {
   return (
     <Modal>
-      <div className={styles.filterModal}>
-        <Button>Search</Button>
-      </div>
+      <form className={styles.filterModal}>
+        <hr />
+        <div className={styles.filterModal__content}>
+          <div className={styles.filterModal__fullTimeSection}>
+            <input type="checkbox" name="" id="full-time-only" />
+            <label htmlFor="full-time-only">Full Time Only</label>
+          </div>
+          <Button>Search</Button>
+        </div>
+      </form>
     </Modal>
   );
 }
