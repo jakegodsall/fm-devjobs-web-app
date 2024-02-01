b89382872a76def4ae839a1b18c953b78f89c78b
jakegodsall <jake.edward.godsall@gmail.com>
2024-02-01 06:53:59 +0000

    created the desktop layout for the FilterBar using media queries

--- frontend/src/app/components/FilterBar/FilterBar.jsx
+++ frontend/src/app/components/FilterBar/FilterBar.jsx
@@ -6,7 +6,10 @@ import searchIcon from "@public/assets/desktop/icon-search.svg";
 import filterIcon from "@public/assets/mobile/icon-filter.svg";
 
 import styles from "./FilterBar.module.css";
+
+import Button from "../UI/FormComponents/Button/Button";
 import TextInput from "../UI/FormComponents/TextInput/TextInput";
+import Checkbox from "../UI/FormComponents/Checkbox/Checkbox";
 
 export default function FilterBar() {
   const handleOnSubmit = (event) => {
@@ -15,19 +18,51 @@ export default function FilterBar() {
 
   return (
     <form onSubmit={handleOnSubmit} className={styles.filterBar}>
-      <TextInput id="filterBar" placeholder="Filter by title..." />
-      <Image
-        className={styles.filterBar__filterIcon}
-        src={filterIcon}
-        alt="filter"
-      />
-      <button className={styles.filterBar__searchIconBackground}>
+      <div className={styles.filterBar__titleSection}>
         <Image
-          className={styles.filterBar__searchIcon}
+          className={styles.filterBar__searchIconDesktop}
           src={searchIcon}
           alt="magnifying glass"
         />
-      </button>
+        <TextInput id="filterBar" placeholder="Filter by title..." />
+        <div className={styles.filterBar__mobileButtons}>
+          <Image
+            className={styles.filterBar__filterIcon}
+            src={filterIcon}
+            alt="filter"
+          />
+          <button className={styles.filterBar__searchIconBackground}>
+            <Image
+              className={styles.filterBar__searchIcon}
+              src={searchIcon}
+              alt="magnifying glass"
+            />
+          </button>
+        </div>
+      </div>
+      <div className={styles.filterBar__locationSection}>
+        <Image
+          className={styles.filterBar__filterIcon}
+          src={filterIcon}
+          alt="filter"
+        />
+        <TextInput id="filterBar" placeholder="Filter by location..." />
+      </div>
+      <div className={styles.filterBar__contractSection}>
+        <div className={styles.filterBar__contractCheckboxContainer}>
+          <Checkbox name="" id="full-time-only" />
+          <label
+            className={styles.filterBar__fullTimeLabel}
+            htmlFor="full-time-only"
+          >
+            Full Time
+          </label>
+        </div>
+        <div className={styles.filterBar__contactButton}>
+
+        <Button>Search</Button>
+        </div>
+      </div>
     </form>
   );
 }
