9645dd563292bd01b113017587b9737d0fe4c075
jakegodsall <jake.edward.godsall@gmail.com>
2024-02-01 07:16:47 +0000

    added basic layout and styling to JobItemCompanyInfo

--- frontend/src/app/jobs/[id]/components/JobItemHeader/JobItemCompanyInfo.jsx
+++ frontend/src/app/jobs/[id]/components/JobItemHeader/JobItemCompanyInfo.jsx
@@ -1,16 +1,26 @@
+import CompanyLogo from "@/app/components/CompanyLogo/CompanyLogo";
 import Button from "@/app/components/UI/FormComponents/Button/Button";
 
-export default function JobItemHeader({
+import styles from "./JobItemCompanyInfo.module.css";
+
+export default function JobItemCompanyInfo({
   logo,
   logoBackground,
   company,
   website,
 }) {
   return (
-    <div>
-      <p>{company}</p>
-      <p>{website}</p>
-      <Button>Company Site</Button>
+    <div className={styles.jobItemCompanyInfo}>
+      <CompanyLogo
+        src={logo}
+        logoBackground={logoBackground}
+        alt={`${company} logo`}
+      />
+      <p className={styles.jobItemCompanyInfo__title}>{company}</p>
+      <p className={styles.jobItemCompanyInfo__website}>{website}</p>
+      <div className={styles.jobItemCompanyInfo__buttonContainer}>
+        <Button>Company Site</Button>
+      </div>
     </div>
   );
 }
