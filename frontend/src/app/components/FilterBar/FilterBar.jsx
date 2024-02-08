7b52c229d9fd7687648d2f7da793266bd45abc89
jakegodsall <jake.edward.godsall@gmail.com>
2024-02-08 06:37:53 +0000

    fixed layout of FilterBar in desktop when refresh icon visible

--- frontend/src/app/components/FilterBar/FilterBar.jsx
+++ frontend/src/app/components/FilterBar/FilterBar.jsx
@@ -94,7 +94,6 @@ export default function FilterBar({
             />
           )}
         </AnimatePresence>
-
         <Image
           className={styles.filterBar__searchIconDesktop}
           src={searchIcon}
